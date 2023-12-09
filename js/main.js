ce4a3cdf58fc8f09155ee7db89a63065ea084b0f
belenviton <belenviton@hotmail.com>
2023-12-09 10:20:25 +0100

    add counter

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const playBtn = document.querySelector('.js__play_button');
 const iptNumber = document.querySelector('.js__input_number');
 const computerMessage = document.querySelector('.js__message');
-
+const counterPc = document.querySelector('.js__counter');
 // FUNCIONES
 
 function getRandomNumber(max) {
@@ -16,31 +16,30 @@ const randomNumber =  getRandomNumber (100);
 const createMessage = () => {
 
    const userNumber = iptNumber.value;
-   iptNumber.max = 100
-   const top = iptNumber.max
-    
-   if (userNumber > top) {
-      computerMessage.innerHTML = `El número debe estar entre 1 y 100`
-   }
-   if (randomNumber >userNumber) {
+  
+  if (userNumber > randomNumber) {
 computerMessage.innerHTML = `Demasiado alto`;
    }
    else if (userNumber < randomNumber) {
    computerMessage.innerHTML = `Demasiado bajo`;
 }
-   else if (userNumber === randomNumber) { 
-   computerMessage.innerHTML = `¡¡¡Has ganado campeona!!!`
+   else { 
+      computerMessage.innerHTML = `¡¡¡Has ganado campeona!!!`
 }
     };
+
+    let numberClicks = 0
+
+const numberTest = () => {
+   counterPc.innerHTML = `Número de intentos: ` + `${numberClicks++}`;
+}
 
   const handleClickBtn = (event) => {
    event.preventDefault ();
    createMessage ();
+   numberTest ();
    console.log(randomNumber);
   }
-
-
-
 
 //Eventos
 playBtn.addEventListener('click', handleClickBtn);
