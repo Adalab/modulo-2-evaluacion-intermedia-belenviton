1603ef8054928020522568c3a5027b4cdff07f71
belenviton <belenviton@hotmail.com>
2023-12-09 11:10:47 +0100

    exercise

--- js/main.js
+++ js/main.js
@@ -12,39 +12,45 @@ function getRandomNumber(max) {
 
 const randomNumber =  getRandomNumber (100);
 
+const win = () => computerMessage.innerHTML = `¡¡¡Has ganado campeona!!!`;
+
+const high = () => computerMessage.innerHTML = `Demasiado alto`;
+
+const short = () => computerMessage.innerHTML = `Demasiado bajo`;
 
 const createMessage = () => {
 
    const userNumber = iptNumber.value;
    iptNumber.max = 100;
    
-  if (userNumber < 1 || userNumber > 100) {
+   if (userNumber < 1 || userNumber > 100) {
    computerMessage.innerHTML = `El número debe estar
    entre 1 y 100.`;
   }
-  else if (userNumber > randomNumber) {
-computerMessage.innerHTML = `Demasiado alto`;
+   else if (userNumber > randomNumber) {
+   high ();
    }
    else if (userNumber < randomNumber) {
-   computerMessage.innerHTML = `Demasiado bajo`;
-}
+   short ();
+   }
    else { 
-      computerMessage.innerHTML = `¡¡¡Has ganado campeona!!!`
-}
+     win ();
+   }
     };
-
-    let numberClicks = 0
+   
+    let numberClicks = 1
 
 const numberTest = () => {
    counterPc.innerHTML = `Número de intentos: ` + `${numberClicks++}`;
-}
+};
 
   const handleClickBtn = (event) => {
    event.preventDefault ();
    createMessage ();
    numberTest ();
    console.log(randomNumber);
-  }
+  };
+
 
 //Eventos
 playBtn.addEventListener('click', handleClickBtn);
